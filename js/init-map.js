86ba855ecec6a235391b54e5751150d0eab5a762
psimonek <kopyta_strelby_0u@icloud.com>
2024-11-09 12:00:06 +0100

    Update init-map.js

--- js/init-map.js
+++ js/init-map.js
@@ -4,7 +4,7 @@ var map = L.map('map', {
     scrollWheelZoom: false,
     smoothWheelZoom: true,
     smoothSensitivity: 5,
-	rotate: true
+	rotate: true,
 }).setView([49.1951, 16.6068], 13); // Souřadnice Brna
 
 // Přidání OpenStreetMap a ostatních dlaždic
