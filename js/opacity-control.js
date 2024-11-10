2b9dcf9f6f628781e563694080a7f97e1283b8ee
psimonek <kopyta_strelby_0u@icloud.com>
2024-11-10 13:20:42 +0100

    Update opacity-control.js

--- js/opacity-control.js
+++ js/opacity-control.js
@@ -4,30 +4,49 @@ function updateOpacity(opacity) {
         layer.setOpacity(opacity);
     });
 }
+
 // Přidání slideru pro průhlednost
 var opacityControl = L.control({
     position: 'topright',    
 });
+
 opacityControl.onAdd = function(map) {
     var opacityContainer = L.DomUtil.create('div', 'leaflet-control-opacity');
     opacityContainer.style.background = 'white';
-    opacityContainer.style.padding = '6px';
-    opacityContainer.style.border = '1px solid #ccc';
+    opacityContainer.style.padding = '0px';
+    opacityContainer.style.border = '2px solid rgba(0, 0, 0, 0.2)';
     opacityContainer.style.borderRadius = '5px';
     opacityContainer.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.2)';
+    opacityContainer.style.display = 'flex';
+    opacityContainer.style.flexDirection = 'column'; // Vertikální orientace
+    opacityContainer.style.width = '48px';
+    opacityContainer.style.height = '124px';
+
     var opacityLabel = L.DomUtil.create('label', '', opacityContainer);
-    opacityLabel.innerHTML = '<span style="text-align: center; font-size: 1.1em;">Průhlednost mapy</span>';
+    //opacityLabel.innerHTML = '<span style="text-align: center; font-size: 1.1em;">Průhl.</span>';
     opacityLabel.style.display = 'block';
     opacityLabel.style.marginBottom = '10px';
+
     var opacitySlider = L.DomUtil.create('input', '', opacityContainer);
     opacitySlider.type = 'range';
     opacitySlider.min = 0;
     opacitySlider.max = 1;
     opacitySlider.step = 0.1;
     opacitySlider.value = 1;
+
+    // Přidání stylů pro vertikální slider
+    opacitySlider.style.writingMode = 'bt-lr'; // Vertikální orientace
+    opacitySlider.style.transform = 'rotate(270deg)'; // Otočení pro správnou orientaci
+    opacitySlider.style.margin = '0 auto'; // Centering
+    opacitySlider.style.marginLeft = '-24px';
+    opacitySlider.style.marginTop = '48px';
+    opacitySlider.style.width = '100px'; // Šířka táhla
+    opacitySlider.style.height = '8px'; // Výška táhla
+
     L.DomEvent.on(opacitySlider, 'input', function() {
         updateOpacity(opacitySlider.value);
     });
+
     L.DomEvent.disableClickPropagation(opacityContainer);
     L.DomEvent.disableScrollPropagation(opacityContainer);
     opacityContainer.appendChild(opacityLabel);
@@ -36,6 +55,5 @@ opacityControl.onAdd = function(map) {
 };
 
 opacityControl.addTo(map);
-
 L.control.layers(baseMaps).addTo(map);
 map.addLayer(osm);
