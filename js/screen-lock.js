eff312b0dc65cf9a5946d16124da5de9a9b05aa3
psimonek <kopyta_strelby_0u@icloud.com>
2024-11-10 09:34:22 +0100

    Update screen-lock.js

--- js/screen-lock.js
+++ js/screen-lock.js
@@ -5,7 +5,6 @@ async function requestWakeLock() {
     try {
         wakeLock = await navigator.wakeLock.request('screen');
         console.log('Wake Lock is active');
-        wakeLock = 1;
     } catch (err) {
         console.error(`${err.name}, ${err.message}`);
     }
