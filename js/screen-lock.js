9a193e9f8aaa824ff8d8c5b282718d4589779ad6
psimonek <kopyta_strelby_0u@icloud.com>
2024-11-10 07:18:32 +0100

    Update screen-lock.js

--- js/screen-lock.js
+++ js/screen-lock.js
@@ -1,50 +1,19 @@
-// test support
-let isSupported = false;
+let wakeLock = null;
 
-if ('wakeLock' in navigator) {
-  isSupported = true;
-} else {
-  console.log('Prohlížeč nepodporuje wakeLock!!!');
+// Definice funkce requestWakeLock
+async function requestWakeLock() {
+    try {
+        wakeLock = await navigator.wakeLock.request('screen');
+        console.log('Wake Lock is active');
+    } catch (err) {
+        console.error(`${err.name}, ${err.message}`);
+    }
 }
 
-if (isSupported) {
-  // create a reference for the wake lock
-  let wakeLock = null;
- console.log('Tady jsme v části isSuported.');
-  // create an async function to request a wake lock
-  const requestWakeLock = async () => {
-    console.log('Tady jsme v části const requestWakeLock.');
-    try {
-      wakeLock = await navigator.wakeLock.request('screen');
-
-      // change up our interface to reflect wake lock active
-      //changeUI();
-
-      // listen for our release event
-      wakeLock.onrelease = function(ev) {
-        console.log(ev);
-      }
-      //wakeLock.addEventListener('release', () => {
-        // if wake lock is released alter the button accordingly
-        //changeUI('released');
-     //});
-
-    } catch (err) {
-      // if wake lock request fails - usually system related, such as battery
-      statusElem.textContent = `${err.name}, ${err.message}`;
-
+// Definice funkce releaseWakeLock
+async function releaseWakeLock() {
+    if (wakeLock !== null) {
+        await wakeLock.release();
+        wakeLock = null;
+        console.log('Wake Lock is released');
     }
-  } // requestWakeLock()
-
-
-  const handleVisibilityChange = () => {
-    if (wakeLock !== null && document.visibilityState === 'visible') {
-      requestWakeLock();
-    }
-  }
-
-  document.addEventListener('visibilitychange', handleVisibilityChange);
-
-
-} // isSupported
-requestWakeLock();
