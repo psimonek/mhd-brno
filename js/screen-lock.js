63c08ef1c7d356f74d92ddb292a8620438b1d63b
psimonek <kopyta_strelby_0u@icloud.com>
2024-11-10 07:43:01 +0100

    Update screen-lock.js

--- js/screen-lock.js
+++ js/screen-lock.js
@@ -1,20 +1,37 @@
 let wakeLock = null;
 
-// Definice funkce requestWakeLock
-async function requestWakeLock() {
+const requestWakeLock = async () => {
     try {
         wakeLock = await navigator.wakeLock.request('screen');
         console.log('Wake Lock is active');
     } catch (err) {
         console.error(`${err.name}, ${err.message}`);
     }
-}
+};
 
-// Definice funkce releaseWakeLock
-async function releaseWakeLock() {
+const releaseWakeLock = async () => {
     if (wakeLock !== null) {
         await wakeLock.release();
         wakeLock = null;
         console.log('Wake Lock is released');
     }
-}
+};
+
+// Funkce pro zpracování změny viditelnosti
+const handleVisibilityChange = async () => {
+    if (document.visibilityState === 'visible') {
+        // Pokud se stránka stane viditelnou, znovu požádejte o wake lock
+        await requestWakeLock();
+    } else {
+        // Pokud stránka není viditelná, uvolněte wake lock
+        await releaseWakeLock();
+    }
+};
+
+// Přidání posluchače události pro změnu viditelnosti
+document.addEventListener('visibilitychange', handleVisibilityChange);
+
+// Volání funkce pro požadavek na Wake Lock při načtení stránky
+document.addEventListener('DOMContentLoaded', async () => {
+    await requestWakeLock();
+});
