b41465e9d691f022eeef14ae2084fa95fc2804e9
psimonek <kopyta_strelby_0u@icloud.com>
2024-11-19 17:21:48 +0100

    Update a.js

--- js/a.js
+++ js/a.js
@@ -13,7 +13,7 @@ function getLocation() {
     if (navigator.geolocation) {
         if (!tracking) {
             // Aktivace sledování polohy
-            map.locate({ setView: true, watch: true, maximumAge: 100 });
+            map.locate({ setView: true, watch: true, maximumAge: 0, enableHighAccuracy: true });
             
             map.on('locationfound', showPosition);
             map.on('locationerror', showError);
